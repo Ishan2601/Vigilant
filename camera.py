fb7048a9d509d0eabefe4606960613531fd8890d
Ishan2601 <apiishan18@gmail.com>
2021-04-10 15:28:24 +0530

    Added Flask Implementation

--- camera.py
+++ camera.py
@@ -6,9 +6,39 @@ import cv2
 
 
 class VideoCamera(object):
-    # Returns EAR given eye landmarks
 
-    def eye_aspect_ratio(eye):
+    def __init__(self):
+        self.eye_thresh = 0.3
+        self.eye_consec_frames = 50
+        self.mouth_thresh = 0.6
+        self.mouth_consec_frames = 50
+
+        self.eye_count = 0
+        self.mouth_count = 0
+        self.yawn_alarm = False
+        self.sleep_alarm = False
+
+        self.WHITE_COLOR = (255, 255, 255)
+        self.YELLOW_COLOR = (0, 255, 255)
+        self.RED_COLOR = (0, 0, 255)
+        self.GREEN_COLOR = (0, 255, 0)
+        self.BLUE_COLOR = (255, 0, 0)
+        self.BLACK_COLOR = (0, 0, 0)
+
+        self.w, self.h = 60, 35
+
+        self.shape_predictor = "model/shape_predictor_68_face_landmarks.dat"
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor(self.shape_predictor)
+
+    # Grab the indexes of the facial landmarks for the left and right eye and mouth respectively
+        (self.lStart, self.lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+        (self.rStart, self.rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+        (self.mStart, self.mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+
+        self.cap = cv2.VideoCapture(0)
+    # Returns EAR given eye landmarks
+    def eye_aspect_ratio(self, eye):
         # Compute the euclidean distances between the two sets of
         # vertical eye landmarks (x, y)-coordinates
         A = np.linalg.norm(eye[1] - eye[5])
@@ -24,9 +54,8 @@ class VideoCamera(object):
     # Return the eye aspect ratio
         return ear
 
-# Returns MAR given eye landmarks
-
-    def mouth_aspect_ratio(mouth):
+    # Returns MAR given eye landmarks
+    def mouth_aspect_ratio(self,mouth):
         # Compute the euclidean distances between the three sets
         # of vertical mouth landmarks (x, y)-coordinates
         A = np.linalg.norm(mouth[13] - mouth[19])
@@ -43,131 +72,68 @@ class VideoCamera(object):
     # Return the mouth aspect ratio
         return mar
 
-    eye_thresh = 0.3
-    eye_consec_frames = 50
-    mouth_thresh = 0.6
-    mouth_consec_frames = 50
+    def get_frame(self):
+        while True:
 
-    eye_count = 0
-    mouth_count = 0
-    yawn_alarm = False
-    sleep_alarm = False
+            _, frame = self.cap.read()
+            frame = imutils.resize(frame, width=480)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    WHITE_COLOR = (255, 255, 255)
-    YELLOW_COLOR = (0, 255, 255)
-    RED_COLOR = (0, 0, 255)
-    GREEN_COLOR = (0, 255, 0)
-    BLUE_COLOR = (255, 0, 0)
-    BLACK_COLOR = (0, 0, 0)
+            rects = self.detector(gray)
+            if len(rects) > 0:
+                rect = rects[0]
+            else:
+#                cv2.imshow("Drowsiness Detection System", frame)
+#                key = cv2.waitKey(1) & 0xFF
+                continue
 
-    w, h = 60, 35
+            shape = self.predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
 
-    shape_predictor = "model/shape_predictor_68_face_landmarks.dat"
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor(shape_predictor)
+            mouth = shape[self.mStart:self.mEnd]
+            leftEye = shape[self.lStart:self.lEnd]
+            rightEye = shape[self.rStart:self.rEnd]
 
-# Grab the indexes of the facial landmarks for the left and right eye and mouth respectively
-    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
-    (mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+            mar = self.mouth_aspect_ratio(mouth)
+            leftEAR = self.eye_aspect_ratio(leftEye)
+            rightEAR = self.eye_aspect_ratio(rightEye)
+            ear = (leftEAR + rightEAR) / 2.0
+            diff_ear = np.abs(leftEAR - rightEAR)
 
-    cap = cv2.VideoCapture(0)
+            mouthHull = cv2.convexHull(mouth)
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+            cv2.drawContours(frame, [mouthHull], -1, self.YELLOW_COLOR, 1)
+            cv2.drawContours(frame, [leftEyeHull], -1, self.YELLOW_COLOR, 1)
+            cv2.drawContours(frame, [rightEyeHull], -1, self.YELLOW_COLOR, 1)
 
-    while True:
+            for (x, y) in np.concatenate((mouth, leftEye, rightEye), axis=0):
+                cv2.circle(frame, (x, y), 2, self.GREEN_COLOR, -1)
 
-        _, frame = cap.read()
-        frame = imutils.resize(frame, width=480)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            if ear < self.eye_thresh:
+                self.eye_count += 1
+                if self.eye_count > self.eye_consec_frames:
+                    if not self.sleep_alarm:
+                        self.sleep_alarm = True
 
-        rects = detector(gray)
-        if len(rects) > 0:
-            rect = rects[0]
-        else:
-            cv2.imshow("Drowsiness Detection System", frame)
-            key = cv2.waitKey(1) & 0xFF
-            continue
+                cv2.putText(frame, "You are Sleepy!!!", (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            else:
+                self.eye_count = 0
+                self.sleep_alarm = False
 
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
+            if mar > self.mouth_thresh:
+                self.mouth_count += 1
+                if self.mouth_count > self.mouth_consec_frames:
+                    if not self.yawn_alarm:
+                        self.yawn_alarm = True
 
-        mouth = shape[mStart:mEnd]
-        leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
+                cv2.putText(frame, "You are Yawning!!!", (10, 60),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            else:
+                self.mouth_count = 0
+                self.yawn_alarm = False
 
-        mar = mouth_aspect_ratio(mouth)
-        leftEAR = eye_aspect_ratio(leftEye)
-        rightEAR = eye_aspect_ratio(rightEye)
-        ear = (leftEAR + rightEAR) / 2.0
-        diff_ear = np.abs(leftEAR - rightEAR)
-
-        mouthHull = cv2.convexHull(mouth)
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        cv2.drawContours(frame, [mouthHull], -1, YELLOW_COLOR, 1)
-        cv2.drawContours(frame, [leftEyeHull], -1, YELLOW_COLOR, 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, YELLOW_COLOR, 1)
-
-        for (x, y) in np.concatenate((mouth, leftEye, rightEye), axis=0):
-            cv2.circle(frame, (x, y), 2, GREEN_COLOR, -1)
-
-        if ear < eye_thresh:
-            eye_count += 1
-            if eye_count > eye_consec_frames:
-                if not sleep_alarm:
-                    sleep_alarm = True
-
-            cv2.putText(frame, "You are Sleepy!!!", (10, 30),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        else:
-            eye_count = 0
-            sleep_alarm = False
-
-        if mar > mouth_thresh:
-            mouth_count += 1
-            if mouth_count > mouth_consec_frames:
-                if not yawn_alarm:
-                    yawn_alarm = True
-
-            cv2.putText(frame, "You are Yawning!!!", (10, 60),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        else:
-            mouth_count = 0
-            yawn_alarm = False
-
-        cv2.imshow("Drowsiness Detection System", frame)
-        key = cv2.waitKey(1) & 0xFF
-
-    # If the `Esc` key was pressed, break from the loop
-        if key == 27:
-            break
-
-# Do a bit of cleanup
-    cv2.destroyAllWindows()
-    cap.release()
-
-#!/usr/bin/env python
-# import cv2
-# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# class VideoCamera(object):
-#     def __init__(self):
-#         # Using OpenCV to capture from device 0. If you have trouble capturing
-#         # from a webcam, comment the line below out and use a video file
-#         # instead.
-#         self.video = cv2.VideoCapture(0)
-#         # If you decide to use video.mp4, you must have this file in the folder
-#         # as the main.py.
-#         # self.video = cv2.VideoCapture('video.mp4')
-
-#     def __del__(self):
-#         self.video.release()
-
-#     def get_frame(self):
-#         success, image = self.video.read()
-#         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-#         # so we must encode it into JPEG in order to correctly display the
-#         # video stream.
-#         faces = face_cascade.detectMultiScale(image, 1.3, 5)
-#         for (x, y, w, h) in faces:
-#             cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-#         ret, jpeg = cv2.imencode('.jpg', image)
-#         return jpeg.tobytes()
+            #cv2.imshow("Drowsiness Detection System", frame)
+            ret, jpeg = cv2.imencode('.jpg', frame)
+            return jpeg.tobytes()
