cd3c1d2f7544e73af90aad63947dcc700cd974fe
Ishan2601 <apiishan18@gmail.com>
2021-04-10 19:55:32 +0530

    Added Night Vision and Firebase

--- camera.py
+++ camera.py
@@ -3,14 +3,14 @@ import numpy as np
 import imutils
 import dlib
 import cv2
-
+from firebase import firebase
 
 class VideoCamera(object):
 
     def __init__(self):
-        self.eye_thresh = 0.3
+        self.eye_thresh = 0.4
         self.eye_consec_frames = 50
-        self.mouth_thresh = 0.6
+        self.mouth_thresh = 0.4
         self.mouth_consec_frames = 50
 
         self.eye_count = 0
@@ -31,13 +31,17 @@ class VideoCamera(object):
         self.detector = dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor(self.shape_predictor)
 
+        self.fbApp = firebase.FirebaseApplication('https://vigilant-73f45-default-rtdb.firebaseio.com/', None)
+
     # Grab the indexes of the facial landmarks for the left and right eye and mouth respectively
         (self.lStart, self.lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-        (self.rStart, self.rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+        (self.rStart,
+         self.rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
         (self.mStart, self.mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
 
         self.cap = cv2.VideoCapture(0)
     # Returns EAR given eye landmarks
+
     def eye_aspect_ratio(self, eye):
         # Compute the euclidean distances between the two sets of
         # vertical eye landmarks (x, y)-coordinates
@@ -55,7 +59,7 @@ class VideoCamera(object):
         return ear
 
     # Returns MAR given eye landmarks
-    def mouth_aspect_ratio(self,mouth):
+    def mouth_aspect_ratio(self, mouth):
         # Compute the euclidean distances between the three sets
         # of vertical mouth landmarks (x, y)-coordinates
         A = np.linalg.norm(mouth[13] - mouth[19])
@@ -74,8 +78,13 @@ class VideoCamera(object):
 
     def get_frame(self):
         while True:
-
             _, frame = self.cap.read()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            contrast = 1.25
+            brightness = 50
+            frame[:, :, 2] = np.clip(
+                contrast * frame[:, :, 2] + brightness, 0, 255)
+            frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
             frame = imutils.resize(frame, width=480)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -115,24 +124,28 @@ class VideoCamera(object):
                 if self.eye_count > self.eye_consec_frames:
                     if not self.sleep_alarm:
                         self.sleep_alarm = True
+                        self.fbApp.put('/vigilant-73f45-default-rtdb/Driver/-MXvinFMl_9RrzJgY70w', 'Sleepy', 'Yes')
 
                 cv2.putText(frame, "You are Sleepy!!!", (10, 30),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
             else:
                 self.eye_count = 0
                 self.sleep_alarm = False
+                self.fbApp.put('/vigilant-73f45-default-rtdb/Driver/-MXvinFMl_9RrzJgY70w', 'Sleepy', 'No')
 
             if mar > self.mouth_thresh:
                 self.mouth_count += 1
                 if self.mouth_count > self.mouth_consec_frames:
                     if not self.yawn_alarm:
                         self.yawn_alarm = True
+                        self.fbApp.put('/vigilant-73f45-default-rtdb/Driver/-MXvinFMl_9RrzJgY70w', 'Yawning', 'Yes')
 
                 cv2.putText(frame, "You are Yawning!!!", (10, 60),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
             else:
                 self.mouth_count = 0
                 self.yawn_alarm = False
+                self.fbApp.put('/vigilant-73f45-default-rtdb/Driver/-MXvinFMl_9RrzJgY70w', 'Yawning', 'No')
 
             #cv2.imshow("Drowsiness Detection System", frame)
             ret, jpeg = cv2.imencode('.jpg', frame)
